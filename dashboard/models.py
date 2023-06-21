c9c436108372e0f0d1ebcd0eccfab8a7976dd713
Shane-Bath <>
2023-06-21 21:27:49 +0000

    Add superuser

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# class users(models.Model):
+#     name = models.CharField(max_length=200)
+#     role = models.CharField(max_lenght=50)
+#     created_on = models.DateTimeField()
+
+
