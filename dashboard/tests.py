c93aff0ae1f82d95199146276fba1da56dc37cca
Shane-Bath <>
2023-06-27 22:43:30 +0000

    Add tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Client, PhoneLog
 
 # Create your tests here.
+
+
+def test_client(self):
+    client = Client.objects.get(id=1)
+    self.assertEqual(client.first_name, 'Shane')
+    self.assertEqual(client.email_address, 'Shane')
+
+
+def test_phone_log(self):
+    self.assertEqual(PhoneLog.first_name, ' ')
