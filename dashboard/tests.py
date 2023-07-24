cfeea88b4da6b139aaa5a2eb3f1d9908148f111d
Shane-Bath <>
2023-07-24 11:40:01 +0000

    Add centering to cal-display

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -3,5 +3,10 @@ from django.test import TestCase, Client
 from .models import Client, PhoneLog
 from django.urls import reverse
 import unittest
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.contrib.staticfiles import finders
 # Create your tests here.
 
+
+class MyTest(unittest.TestCase):
+    self.assertTrue(staticfiles_storage.exists(finders.find(static/css/style.css')))
