a1f2b88602a88e2ad2f90217bb4a8de4cc73b048
Shane-Bath <>
2023-07-29 11:17:08 +0000

    Add Bootstrap success toast and javascript file

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+console.log('is the JS file linked?');
+
+
+// Succcess message toast
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl);
+    });
+    toastList.forEach(toast => toast.show());
+});
